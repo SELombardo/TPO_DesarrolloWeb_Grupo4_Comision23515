5ddfaeea17a8153415d3c67620708f923764356f
ariosmen <arielosmen@hotmail.com>
2023-09-28 11:56:06 -0300

    Listo el menu responsive y los scripts

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+// MENU RESPONSIVE
+
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
--- js/scriptinicio.js
+++ js/scriptinicio.js
@@ -1,3 +1,5 @@
+// CUADRO DE MENSAJES ALEATORIOS
+
 const mensajes = [
   "El gimnasio es el lugar donde tus metas se vuelven posibles. ¡Sigue adelante y alcanza tus objetivos",
   "Cada día en el gimnasio es un paso más cerca de ser la mejor versión de ti mismo. ¡No pares ahora!",
@@ -12,24 +14,13 @@ function mostrarMensaje() {
   const mensajeContainer = document.getElementById("mensaje-container");
   mensajeContainer.textContent = mensajes[mensajeActual];
 
-  // Incrementa el índice del mensaje actual
   mensajeActual++;
 
-  // Si llegamos al final del arreglo de mensajes, reiniciamos al principio
   if (mensajeActual === mensajes.length) {
     mensajeActual = 0;
   }
 
-  // Llama a la función de mostrarMensaje después de 2 segundos (2000 milisegundos)
-  setTimeout(mostrarMensaje, 5000); // Cambia el mensaje cada 2 segundos
+  setTimeout(mostrarMensaje, 5000); 
 }
 
-// Inicia el proceso mostrando el primer mensaje
 mostrarMensaje();
-
-const burger = document.querySelector(".burger");
-const navLinks = document.querySelector(".nav-links");
-
-burger.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-});
