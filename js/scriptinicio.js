1207cd5ea9888ecacdf7cbfa664e9ec0e51b0206
Santiago Lombardo <lombardo.s.e@gmail.com>
2023-10-01 17:44:05 -0300

    prueba de fonts hero y correcciones varias

--- js/scriptinicio.js
+++ js/scriptinicio.js
@@ -1,11 +1,11 @@
 // CUADRO DE MENSAJES ALEATORIOS
 
 const mensajes = [
-  "El gimnasio es el lugar donde tus metas se vuelven posibles. ¡Sigue adelante y alcanza tus objetivos",
-  "Cada día en el gimnasio es un paso más cerca de ser la mejor versión de ti mismo. ¡No pares ahora!",
-  "El dolor que sientes hoy será la fuerza que sientas mañana. ¡Sigue entrenando con determinación!",
-  "Tu cuerpo es tu proyecto más importante. Invierte tiempo en él, ¡los resultados valen la pena",
-  "El gimnasio no solo transforma tu cuerpo, también fortalece tu mente. ¡Mantén una actitud positiva y nunca te rindas!",
+  '"El gimnasio es el lugar donde tus metas se vuelven posibles. ¡Sigue adelante y alcanza tus objetivos"',
+  '"Cada día en el gimnasio es un paso más cerca de ser la mejor versión de ti mismo. ¡No pares ahora!"',
+  '"El dolor que sientes hoy será la fuerza que sientas mañana. ¡Sigue entrenando con determinación!"',
+  '"Tu cuerpo es tu proyecto más importante. Invierte tiempo en él, ¡los resultados valen la pena"',
+  '"El gimnasio no solo transforma tu cuerpo, también fortalece tu mente. ¡Mantén una actitud positiva y nunca te rindas!"',
 ];
 
 let mensajeActual = 0;
